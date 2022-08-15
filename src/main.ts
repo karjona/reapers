3a233e0117c3b5e04e32da3b20dd3a713150684f
Kilian Arjona <23211440+karjona@users.noreply.github.com>
2022-08-15 19:45:12 +0200

    Set up the game canvas

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,32 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { init, Sprite, GameLoop } from "kontra";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const { canvas } = init();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const sprite = Sprite({
+  x: 100, // starting x,y position of the sprite
+  y: 80,
+  color: "red", // fill color of the sprite rectangle
+  width: 20, // width and height of the sprite rectangle
+  height: 40,
+  dx: 2, // move the sprite 2px to the right every frame
+});
+
+const gameloop = GameLoop({
+  // create the main game loop
+  update: function () {
+    // update the game state
+    sprite.update();
+
+    // wrap the sprites position when it reaches
+    // the edge of the screen
+    if (sprite.x > canvas.width) {
+      sprite.x = -sprite.width;
+    }
+  },
+  render: function () {
+    // render the game state
+    sprite.render();
+  },
+});
+
+gameloop.start(); // start the game
