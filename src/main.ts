692de4e1b9d8d60b20d32ee0ffbc0768b8e6c46d
Kilian Arjona <23211440+karjona@users.noreply.github.com>
2022-08-15 22:04:52 +0200

    Set starting player positions

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,52 @@
-import { init, Sprite, GameLoop } from "kontra";
+import { init, Sprite, GameLoop, initKeys, keyPressed } from "kontra";
 
 const { canvas } = init();
+initKeys();
 
-const sprite = Sprite({
-  x: 100, // starting x,y position of the sprite
-  y: 80,
-  color: "red", // fill color of the sprite rectangle
-  width: 20, // width and height of the sprite rectangle
-  height: 40,
-  dx: 2, // move the sprite 2px to the right every frame
+const playerWidth = 48;
+const playerHeight = 48;
+const playerWalkSpeed = 1;
+const groundLevel = 120;
+
+const player1 = Sprite({
+  x: Math.round(canvas.width / 3) - Math.round(playerWidth / 2),
+  y: groundLevel - playerHeight,
+  color: "red",
+  width: playerWidth,
+  height: playerHeight,
+});
+
+const player2 = Sprite({
+  x: Math.round((canvas.width * 2) / 3) - Math.round(playerWidth / 2),
+  y: groundLevel - playerHeight,
+  color: "pink",
+  width: playerWidth,
+  height: playerHeight,
 });
 
 const gameloop = GameLoop({
-  // create the main game loop
   update: function () {
-    // update the game state
-    sprite.update();
+    const isMovingRight = ["arrowright", "d"].some(keyPressed);
+    const isMovingLeft = ["arrowleft", "a"].some(keyPressed);
+    player1.dx = isMovingLeft
+      ? -playerWalkSpeed
+      : isMovingRight
+      ? playerWalkSpeed
+      : 0;
 
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width;
+    player1.update();
+    player2.update();
+
+    if (player1.x + player1.width > canvas.width) {
+      player1.x = canvas.width - playerWidth;
+    } else if (player1.x < 0) {
+      player1.x = 0;
     }
   },
   render: function () {
-    // render the game state
-    sprite.render();
+    player1.render();
+    player2.render();
   },
 });
 
-gameloop.start(); // start the game
+gameloop.start();
