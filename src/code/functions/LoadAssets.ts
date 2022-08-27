e98a26bfeafcbc3a13e1fafd11f3891001a74b9c
Kilian Arjona <23211440+karjona@users.noreply.github.com>
2022-08-27 20:44:15 +0200

    Update image reference for bundling

--- src/code/functions/LoadAssets.ts
+++ src/code/functions/LoadAssets.ts
@@ -1,8 +1,10 @@
 import { loadImage } from "kontra";
+import player1webp from "/src/images/players/player1.webp";
+import player2webp from "/src/images/players/player2.webp";
 
 export async function LoadAssets() {
-  const player1Image = await loadImage("/src/images/players/player1.webp");
-  const player2Image = await loadImage("/src/images/players/player2.webp");
+  const player1Image = await loadImage(player1webp);
+  const player2Image = await loadImage(player2webp);
 
   return {
     player1Image,
