689fb3f67f9d885d6c052192e68ffea576552bf7
Kilian Arjona <23211440+karjona@users.noreply.github.com>
2022-08-29 12:35:54 +0200

    Center announcer text

--- src/code/modules/AnnouncerText/AnnouncerText.ts
+++ src/code/modules/AnnouncerText/AnnouncerText.ts
@@ -1,10 +1,11 @@
+import { canvas } from "../../data/Instances";
 import { GameObject } from "kontra";
 import { renderText } from "../../data/Instances";
 
 let label = "";
 
 export const AnnouncerText = GameObject({
-  x: 64,
+  x: 0,
   y: 75,
   render: function () {
     renderText(label, 0, 0, 5, "white");
@@ -12,5 +13,8 @@ export const AnnouncerText = GameObject({
 });
 
 export function setAnnouncerText(text: string) {
+  const textWidth = text.length * 5;
+  const labelXCoord = Math.round(canvas.width / 2 - textWidth / 2);
+  AnnouncerText.x = labelXCoord;
   label = text;
 }
