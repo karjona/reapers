5cbb991c67271614a9d301c5201db9c32fd2981f
Kilian Arjona <23211440+karjona@users.noreply.github.com>
2022-09-03 13:54:43 +0200

    Implement all movement collisions

--- src/code/functions/CheckFighterCollisions.ts
+++ src/code/functions/CheckFighterCollisions.ts
@@ -6,12 +6,36 @@ import { GameConfig } from "../data/GameConfig";
 export default function CheckFighterCollisions() {
   // Player collisions
   if (collides(player1.hitbox, player2.hitbox)) {
-    if (player1.hitbox.dx > 0 && player2.hitbox.dx === 0) {
+    // Player 1 is moving, player 2 is not
+    if (player1.hitbox.dx != 0 && player2.hitbox.dx === 0) {
+      // Push player 2 back
       if (player2.hitbox.x + player2.hitbox.width < canvas.width) {
         player2.hitbox.x += fighterWalkSpeed;
       }
+
+      // Don't let player 1 move past player 2 in corner
       if (player2.hitbox.x + player2.hitbox.width === canvas.width) {
-        player1.hitbox.x = player1.hitbox.x - 1;
+        player1.hitbox.x -= fighterWalkSpeed;
+      }
+
+      // Player 2 is moving, player 1 is not
+    } else if (player2.hitbox.dx != 0 && player1.hitbox.dx === 0) {
+      // Push player 1 back
+      if (player1.hitbox.x > 0) {
+        player1.hitbox.x -= fighterWalkSpeed;
+      }
+
+      // Don't let player 2 move past player 1 in corner
+      if (player1.hitbox.x === 0) {
+        player2.hitbox.x += fighterWalkSpeed;
+      }
+
+      // Both players are colliding, and both are moving or aren't moving at all
+    } else {
+      // If both players are pushing each other, don't move them
+      if (player1.hitbox.dx > 0 && player2.hitbox.dx < 0) {
+        player1.hitbox.x += -fighterWalkSpeed;
+        player2.hitbox.x += fighterWalkSpeed;
       }
     }
   }
